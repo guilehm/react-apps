b26362c8436de31cf748624f6ba0bbbced935da6
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 00:02:04 -0300

    import Todos to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+        <Todos />
     </div>
   );
 }
