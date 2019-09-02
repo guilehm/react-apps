42b6be050ca7ad9222fd51740c49d69d1b20ba15
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 20:26:51 -0300

    create style for todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -21,6 +21,7 @@ export class TodoItem extends Component {
                     <input type="checkbox" 
                     onChange={this.props.markComplete.bind(this, id)} /> {' '}
                     {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
         )
@@ -31,5 +32,14 @@ TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
 }
 
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right',
+}
 
 export default TodoItem
