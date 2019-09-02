bb1bf0acc504fa2ac48464c18da3fd8a1db20cad
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 00:43:25 -0300

    set state

--- src/App.js
+++ src/App.js
@@ -26,7 +26,12 @@ class App extends Component {
 
 
   markComplete = (id) => {
-    console.log(id)
+    this.setState({ todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })})
 }
 
 
