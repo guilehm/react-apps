a51aaab306f586793c13ddc6096ad25da90705cf
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 20:46:39 -0300

    remove hardcoded ids and set uuid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -9,17 +10,17 @@ class App extends Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuid.v4(),
                 title: 'Take out the trash',
                 completed: false
             },
             {
-                id: 2,
+                id: uuid.v4(),
                 title: 'Play games',
                 completed: true
             },
             {
-                id: 3,
+                id: uuid.v4(),
                 title: 'Dinner with girlfriend',
                 completed: false
             }
@@ -48,7 +49,7 @@ class App extends Component {
     // Add Todo
     addTodo = (title) => {
         const newTodo = {
-            id: 4,
+            id: uuid.v4(),
             title,
             completed: false,
         }
