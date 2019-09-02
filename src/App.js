0f31532854c55f56807058daacb72422d7934e56
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 00:07:24 -0300

    refactor from function to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-        <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Play games',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Dinner with girlfriend',
+        completed: false
+      },
+    ]
+  }
+
+
+  render () {
+    return (
+      console.log(this.state.todos),
+      <div className="App">
+        <h1>App</h1>
+          <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
