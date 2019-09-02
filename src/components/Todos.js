36096329ce17e692d598660d6187246c1946440d
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 00:21:18 -0300

    creat todo item

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
     render() {
         return this.props.todos.map((todo) => (
-            <h3>{ todo.title }</h3>
+            <TodoItem todo={todo} />
         ));
     }
 }
