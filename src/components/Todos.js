f46210f2e5d0be4f1bb21952bd6f5b99ffd6183b
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 00:07:03 -0300

    update todo component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Todos() {
-  return (
-    <div className="App">
-      <h1>Todos</h1>
-    </div>
-  );
+class Todos extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Todos</h1>
+            </div>
+        );
+    }
 }
 
 export default Todos;
