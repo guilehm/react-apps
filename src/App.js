0b09044595952eea8c1ccf266abb53f662145930
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 21:06:42 -0300

    set state with axios

--- src/App.js
+++ src/App.js
@@ -5,28 +5,18 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: uuid.v4(),
-                title: 'Take out the trash',
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: 'Play games',
-                completed: true
-            },
-            {
-                id: uuid.v4(),
-                title: 'Dinner with girlfriend',
-                completed: false
-            }
-        ]
+        todos: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({ todos: res.data }))
     }
 
     // Toggle Complete
@@ -55,7 +45,7 @@ class App extends Component {
             title,
             completed: false,
         }
-        this.setState({ todos: [...this.state.todos, newTodo]});
+        this.setState({ todos: [...this.state.todos, newTodo] });
     }
 
 
