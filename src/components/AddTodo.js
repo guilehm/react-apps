ede2815b32f216ae2987505224b817b1dc77a3ab
Guilherme Luis <guile.hm@hotmail.com>
2019-09-02 21:17:24 -0300

    improve prototypes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
     state = {
@@ -36,5 +37,10 @@ export class AddTodo extends Component {
         )
     }
 }
+
+AddTodo.propTypes = {
+    addTodo: PropTypes.func.isRequired,
+}
+
 
 export default AddTodo;
