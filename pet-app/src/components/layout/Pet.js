33387f7a16645acc9bfac3ffb15fbf196051a39f
Guilherme Luis <guile.hm@hotmail.com>
2019-09-08 17:55:45 -0300

    create pet component

--- pet-app/src/components/layout/Pet.js
+++ pet-app/src/components/layout/Pet.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Pet() {
-    return (
-        <div class="card" style={cardStyle}>
-            <img class="card-img-top" src="" alt="pet name" />
-            <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">Pet description.</p>
-                <a href="#" class="btn btn-primary">pet link</a>
+
+
+class Pet extends Component {
+
+    getPetMarkup() {
+        return this.props.pets.map((pet) => (
+            <div key={pet.id} className="card" style={cardStyle}>
+                <img className="card-img-top" src="" alt={pet.name} />
+                <div className="card-body">
+                    <h5 className="card-title">{pet.name}</h5>
+                    <p className="card-text">{pet.description}</p>
+                    <a href="#" className="btn btn-primary">{pet.link}</a>
+                </div>
+            </div>
+        ));
+    }
+
+    render() {
+        return (<div className="row">
+            <div className="col-sm-6">
+                {this.getPetMarkup()}
             </div>
         </div>
-    )
+        )
+    }
 }
 
-
 const cardStyle = {
-    width: '18rem',
+    // width: '18rem',
 }
 
 export default Pet;
