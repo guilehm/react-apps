9cbaa5ded471250d8f7d8df1651e9fe84060b5f8
Guilherme Luis <guile.hm@hotmail.com>
2019-09-08 17:55:54 -0300

    create states

--- pet-app/src/App.js
+++ pet-app/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Pet from './components/layout/Pet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Pet />
-    </div>
-  );
+class App extends Component {
+    state = {
+        pets: [
+            {id: 1, name: 'João', description: 'Descrição do João', link: 'link do joão'},
+            {id: 2, name: 'Maria', description: 'Descrição do Maria', link: 'link do Maria'},
+        ]
+    }
+    render() {
+        return (
+            <div className="App">
+                <Header />
+                <Pet pets={this.state.pets} />
+            </div>
+        );
+    }
 }
 
 export default App;
