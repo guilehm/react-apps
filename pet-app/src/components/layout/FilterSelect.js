1cc4618f55f5b551bd39469e42ab022508b72ea3
guilherme <guile.hm@hotmail.com>
2019-09-14 01:59:04 -0300

    improve get options markup

--- pet-app/src/components/layout/FilterSelect.js
+++ pet-app/src/components/layout/FilterSelect.js
@@ -4,9 +4,16 @@ import React, { Component } from 'react';
 class FilterSelect extends Component {
 
     getOptionsMarkup(options) {
+
         let optionsMarkup = [];
-        for (let o of Object.entries(options)) {
-            optionsMarkup.push(<option key={o[0]} value={o[0]}>{o[1]}</option>)
+        if (Array.isArray(options)) {
+            for (let o of options) {
+                optionsMarkup.push(<option key={o} value={o}>{o}</option>)
+            }
+        } else {
+            for (let o of Object.entries(options)) {
+                optionsMarkup.push(<option key={o[0]} value={o[0]}>{o[1]}</option>)
+            }
         }
         return optionsMarkup;
     }
@@ -15,7 +22,7 @@ class FilterSelect extends Component {
         return (
             <React.Fragment>
                 <label>{this.props.label}</label>
-                <select name={this.props.name} 
+                <select name={this.props.name}
                         onChange={this.props.filterFunction}
                         value={this.props.activeValue}>
                     {this.getOptionsMarkup(this.props.options)}
