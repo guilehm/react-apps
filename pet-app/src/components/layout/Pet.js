1c6fe4c6fcfdcdebd2992e4eddce768e8bda0147
guilherme <guile.hm@hotmail.com>
2019-09-13 23:23:55 -0300

    validate main picture to avoid errors

--- pet-app/src/components/layout/Pet.js
+++ pet-app/src/components/layout/Pet.js
@@ -8,7 +8,9 @@ class Pet extends Component {
         let url = 'https://gui-pets.herokuapp.com/api/pets/';
         return this.props.pets.map((pet) => (
             <div key={pet.id} className="card" style={cardStyle}>
-                <img className="card-img-top" src={pet.mainPicture.url} alt={pet.name} />
+                <img className="card-img-top" 
+                    src={pet.mainPicture ? pet.mainPicture.url : ''} 
+                    alt={pet.name} />
                 <div className="card-body">
                     <h5 className="card-title">{pet.name}</h5>
                     <p className="card-text">{pet.description}</p>
