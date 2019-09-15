8ac21aa262980ae2c1febd1e437d15e67d6d6a54
guilherme <guile.hm@hotmail.com>
2019-09-15 15:28:33 -0300

    create pet detail endpoint

---
+++ pet-app/src/components/PetDetail.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const REACT_APP_BASEURL = process.env.REACT_APP_BASEURL || 'http://localhost:8000';
+
+
+class PetDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            petId: props.match.params.id,
+        }
+    }
+
+
+    componentDidMount() {
+        axios.get(`${REACT_APP_BASEURL}/api/pets/${this.state.petId}/`)
+            .then(res => this.setState(res.data))
+            .catch((e => console.log(e)))
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div className="container-fluid">
+                <h1>{this.state.name}</h1>
+                <p>{this.state.data}</p>
+            </div>
+        )
+    }
+}
+
+export default PetDetail;
