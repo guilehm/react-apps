88480bc2b704ae25348faef80a27870f3778691a
Guilherme Luis <guile.hm@hotmail.com>
2019-09-22 13:31:15 -0300

    change from function to class

--- fortnite-app/src/App.js
+++ fortnite-app/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
-    );
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World!</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
