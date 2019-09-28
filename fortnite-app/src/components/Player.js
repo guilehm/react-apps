f7d1604127dbcea3e3b121b4ca9fff2dffdb8b88
guilherme <guile.hm@hotmail.com>
2019-09-28 12:19:33 -0300

    add state

--- fortnite-app/src/components/Player.js
+++ fortnite-app/src/components/Player.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 const REACT_APP_BASEURL = process.env.REACT_APP_BASEURL || 'http://localhost:4000';
 
 class Player extends Component {
+    state = {
+        playerId: 'AQUACAGNjhTuxlU8SrIBP8eO+O0A',
+        stats: [],
+    }
 
     render() {
         return (
