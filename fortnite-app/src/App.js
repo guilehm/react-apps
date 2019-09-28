af77ecd6a9b8aef0a79d22c86dadd8e5a1e5b897
guilherme <guile.hm@hotmail.com>
2019-09-28 13:57:40 -0300

    improve app

--- fortnite-app/src/App.js
+++ fortnite-app/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Player from './components/Player';
+import axios from 'axios';
+
 import './App.css';
 
+const REACT_APP_BASEURL = process.env.REACT_APP_BASEURL || 'http://localhost:4000';
+
+
 class App extends Component {
+
+    state = {
+        friends: []
+    }
+
+    fetchFriends() {
+        let url = `${REACT_APP_BASEURL}/friends/`;
+        axios.get(url)
+            .then(res => this.setState({ friends: res.data }))
+            .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        this.fetchFriends()
+    }
 
     render() {
         return (
             <div className="App">
-                <h1>Hello World!</h1>
-                <Player />
+                <h1>Friends</h1>
+                {this.state.friends
+                    .map(friend => <Player
+                            key={friend.playerId}
+                            playerId={friend.playerId}
+                            username={friend.handle} />
+                    )}
             </div>
         )
     }
