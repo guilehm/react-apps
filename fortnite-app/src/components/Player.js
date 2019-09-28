ba253a9b0e559d23f9a92aaafcace9a60e227103
guilherme <guile.hm@hotmail.com>
2019-09-28 12:19:13 -0300

    import axios and add environment variable

--- fortnite-app/src/components/Player.js
+++ fortnite-app/src/components/Player.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+const REACT_APP_BASEURL = process.env.REACT_APP_BASEURL || 'http://localhost:4000';
 
 class Player extends Component {
 
