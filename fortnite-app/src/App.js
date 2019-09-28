f44111be51432b5ec745bf6def83b509234652ae
guilherme <guile.hm@hotmail.com>
2019-09-28 11:11:45 -0300

    import player

--- fortnite-app/src/App.js
+++ fortnite-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Player from './components/Player';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hello World!</h1>
+                <Player />
             </div>
         )
     }
