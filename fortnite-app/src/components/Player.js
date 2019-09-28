4dc5a4cdcf9c8adf12a17a6b3087458025e3f28d
guilherme <guile.hm@hotmail.com>
2019-09-28 11:07:58 -0300

    create simple player component

---
+++ fortnite-app/src/components/Player.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Player extends Component {
+
+    render() {
+        return (
+            <div>Hello from Player</div>
+        )
+    }
+}
+
+export default Player;
