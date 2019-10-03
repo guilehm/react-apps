bc410c76f73e8e2e7b5d9efa186cd22f86b96457
Guilherme Luis <guile.hm@hotmail.com>
2019-10-02 23:40:09 -0300

    create fetch character function

--- marvel-api/src/components/Character.js
+++ marvel-api/src/components/Character.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import marvel from '../services/api-configurator'
 
 class Character extends Component {
+
+    state = {
+        characterId: this.props.characterId || '1009351',
+    }
+
+    componentDidMount() {
+        this.fetchCharacter()
+    }
+
+    fetchCharacter() {
+        let url = `${marvel.baseUrl}/characters/${this.state.characterId}/`
+        url = `${url}?${marvel.getAuthParams()}`
+        axios.get(url)
+            .then(res => this.setState(res.data.results[0]))
+            .catch(err => console.log(err))
+    }
 
     render() {
         return (
