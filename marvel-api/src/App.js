d2fbd4ed5d0ad8eef994249be9cc9b617de6e52c
Guilherme Luis <guile.hm@hotmail.com>
2019-10-02 22:54:35 -0300

    create marvel app

---
+++ marvel-api/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Characters</h1>
+            </div>
+        )
+    }
+}
+
+export default App
