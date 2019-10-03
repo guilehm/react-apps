56ff369de2beef582771e0303b00e1ca0d92c16f
Guilherme Luis <guile.hm@hotmail.com>
2019-10-02 23:14:16 -0300

    create get auth data method

--- marvel-api/src/services/api-configurator-.js
+++ marvel-api/src/services/api-configurator-.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 const REACT_APP_PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY
 const REACT_APP_PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY
 
@@ -14,4 +16,17 @@ class Marvel {
         this.apiVersion=apiVersion
     }
 
+    getAuthData() {
+        let timestamp = +new Date()
+        let formatedString = `${timestamp}${this.privateKey}${this.publicKey}`
+        let hash = crypto.createHash('md5').update(formatedString).digest("hex")
+        return {
+            ts: timestamp,
+            apikey: this.publicKey,
+            hash: hash
+        }
+    }
+
 }
+
+export default new Marvel()
