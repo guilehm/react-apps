7f2ef4553bfd3cbf91f489d6c16edda4e9da035e
Guilherme Luis <guile.hm@hotmail.com>
2019-10-12 14:00:44 -0300

    fixes for lint

--- marvel-api/src/services/api-configurator.js
+++ marvel-api/src/services/api-configurator.js
@@ -7,20 +7,20 @@ const REACT_APP_PRIVATE_KEY = process.env.REACT_APP_PRIVATE_KEY
 class Marvel {
 
     constructor(
-        privateKey=REACT_APP_PRIVATE_KEY,
-        publicKey=REACT_APP_PUBLIC_KEY,
-        apiVersion='v1',
+        privateKey = REACT_APP_PRIVATE_KEY,
+        publicKey = REACT_APP_PUBLIC_KEY,
+        apiVersion = 'v1',
     ) {
-        this.privateKey=privateKey
-        this.publicKey=publicKey
-        this.apiVersion=apiVersion
-        this.baseUrl=`http://gateway.marvel.com/${this.apiVersion}/public`
+        this.privateKey = privateKey
+        this.publicKey = publicKey
+        this.apiVersion = apiVersion
+        this.baseUrl = `http://gateway.marvel.com/${this.apiVersion}/public`
     }
 
     getAuthData() {
         let timestamp = +new Date()
         let formatedString = `${timestamp}${this.privateKey}${this.publicKey}`
-        let hash = crypto.createHash('md5').update(formatedString).digest("hex")
+        let hash = crypto.createHash('md5').update(formatedString).digest('hex')
         return {
             ts: timestamp,
             apikey: this.publicKey,
