54fe78ea90d8810c1c8e7c8e0737b3138942608c
Guilherme Luis <guile.hm@hotmail.com>
2019-10-14 21:41:24 -0300

    remove page from app

--- marvel-api/src/App.js
+++ marvel-api/src/App.js
@@ -9,10 +9,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="page">
-                    <ul className="characters"><CharacterList /></ul>
-
-                </div>
+                <CharacterList />
             </div>
         )
     }
