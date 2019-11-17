b4596343e6f0dde580c49c780c2c0ba2b6fec9b0
Guilherme Luis <guile.hm@hotmail.com>
2019-11-17 13:27:03 -0300

    simplify selector

--- games-app/src/selectors/index.js
+++ games-app/src/selectors/index.js
@@ -1,17 +1,8 @@
 import { createSelector } from 'reselect'
-import { SHOW_ALL } from '../constants/GameFilter'
-
 
 const getGames = state => state.games
 
 export const getVisibleGames = createSelector(
     getGames,
-    (visibilityFilter, games) => {
-        switch (visibilityFilter) {
-            case SHOW_ALL:
-                return games
-            default:
-                throw new Error('Unknown filter: ' + visibilityFilter)
-        }
-    }
+    games => games
 )
