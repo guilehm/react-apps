4e91552278b29211b7fe35278f1c2eb91ad28b9d
Guilherme Luis <guile.hm@hotmail.com>
2019-11-17 11:21:24 -0300

    create search component

---
+++ games-app/src/components/layout/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <input
+            type="text"
+            placeholder="Enter game name..."
+            autoFocus
+        />
+    )
+}
+
+export default Search
