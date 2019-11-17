326ea1f21be03b129650c99c6fcedcf41709dc35
Guilherme Luis <guile.hm@hotmail.com>
2019-11-17 16:33:48 -0300

    create handlesubmit function

--- games-app/src/components/layout/Search.jsx
+++ games-app/src/components/layout/Search.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
 
-const Search = () => {
+const Search = ({ actions }) => {
+    function handleSubmit(e) {
+        const name = e.target.value.trim()
+        actions.addGame({ name: name })
+    }
+
     return (
         <input
             type="text"
             placeholder="Enter game name..."
             autoFocus
+            onBlur={handleSubmit}
         />
     )
 }
