a6123caea1f6008887d8da19ec0e5c132ee45295
Guilherme Luis <guile.hm@hotmail.com>
2019-11-17 11:33:17 -0300

    create provider, store and fake reducer

--- games-app/src/components/layout/Home.jsx
+++ games-app/src/components/layout/Home.jsx
@@ -3,9 +3,7 @@ import Search from './Search'
 
 const Home = () => {
     return (
-        <div>
-            <Search />
-        </div>
+        <Search />
     )
 }
 
--- games-app/src/index.js
+++ games-app/src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const reducer = () => ({})
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
