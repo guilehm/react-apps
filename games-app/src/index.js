7fc889ca61be8694c9537fee356c817746de0015
Guilherme Luis <guile.hm@hotmail.com>
2019-11-18 20:11:09 -0300

    add imports

--- games-app/src/index.js
+++ games-app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-
+import storage from 'redux-persist/lib/storage'
 import reducer from './reducers'
+import { persistReducer, persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 const persistConfig = {
