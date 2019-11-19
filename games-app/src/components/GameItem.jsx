5c7cc0a1114175dde0008af49cdc484b854a8593
guilherme <guile.hm@hotmail.com>
2019-11-19 16:32:35 -0300

    improve game item component to fetch from the api

--- games-app/src/components/GameItem.jsx
+++ games-app/src/components/GameItem.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Rawg from '../services/api'
+import { useEffect } from 'react'
 
 
-const Game = (props) => {
+const api = new Rawg()
+
+const Game = props => {
+
+    useEffect(() => {
+        api.getGameData(3498)
+            .then(handleSuccess)
+            .catch(handleError)
+    }, [])
+
+    let handleError = error => {
+        console.log(error.message)
+    }
+
+    let handleSuccess = response => {
+        console.log(response.data)
+        return response.data
+    }
+
     return (
-        <span>{props.name}</span>
+        <React.Fragment>
+            <span>{props.name}</span>
+        </React.Fragment>
     )
 }
 
