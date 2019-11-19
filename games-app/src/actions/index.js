92b861ac0291ecae4643041082ac2834cde71a2b
guilherme <guile.hm@hotmail.com>
2019-11-19 17:26:18 -0300

    create populate game action

--- games-app/src/actions/index.js
+++ games-app/src/actions/index.js
@@ -2,3 +2,4 @@ import * as types from '../constants/ActionTypes'
 
 
 export const addGame = game => ({ type: types.ADD_GAME, game })
+export const populateGame = (game, data) => ({ type: types.POPULATE_GAME, game, data })
