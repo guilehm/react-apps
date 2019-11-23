3cd861211f17aea6ea43c3038683e30852e5eb13
guilherme <guile.hm@hotmail.com>
2019-11-23 00:22:45 -0300

    add layout reducer to index

--- games-app/src/reducers/index.js
+++ games-app/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import games from './games'
+import layout from './layout'
 
 
 const rootReducer = combineReducers({
     games,
+    layout,
 })
 
 export default rootReducer
