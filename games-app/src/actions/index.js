ab7cb5a68736cd2cf20ccc7b7201570978185f77
guilherme <guile.hm@hotmail.com>
2019-11-23 19:39:46 -0300

    adapt actions

--- games-app/src/actions/index.js
+++ games-app/src/actions/index.js
@@ -6,24 +6,31 @@ const API = new Rawg()
 
 export const setLoading = () => ({ type: types.SET_LOADING })
 export const unsetLoading = () => ({ type: types.SET_LOADING })
-
 export const deleteGame = id => ({ type: types.DELETE_GAME, id })
-
-export const addGame = game => {
-    let handleSuccess = res => {
-        let id = res.data.results[0].id
-        return API.getGameData(id)
-    }
-
+export const addSearchItems = game => {
     return dispatch => {
         API.searchByName(game.name)
-            .then(handleSuccess)
             .then(res => dispatch({
-                type: types.ADD_GAME,
-                payload: res.data
-            }))
-            .catch(err => dispatch({
-                type: types.SET_ERROR
+                type: types.ADD_SEARCH_ITEMS,
+                payload: res.data.results,
             }))
     }
 }
+// export const addGame = game => {
+//     let handleSuccess = res => {
+//         let id = res.data.results[0].id
+//         return API.getGameData(id)
+//     }
+
+//     return dispatch => {
+//         API.searchByName(game.name)
+//             .then(handleSuccess)
+//             .then(res => dispatch({
+//                 type: types.ADD_GAME,
+//                 payload: res.data
+//             }))
+//             .catch(err => dispatch({
+//                 type: types.SET_ERROR
+//             }))
+//     }
+// }
