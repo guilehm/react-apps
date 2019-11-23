319690b03f2347319f148075d89cbd610eb3934f
guilherme <guile.hm@hotmail.com>
2019-11-23 10:20:04 -0300

    set initial state and fix set error case

--- games-app/src/reducers/layout.js
+++ games-app/src/reducers/layout.js
@@ -4,19 +4,23 @@ import {
     SET_ERROR,
 } from '../constants/ActionTypes'
 
+const INITIAL_STATE = {
+    loading: null,
+    error: null,
+}
 
-export default function layout(state = null, action) {
+export default function layout(state = INITIAL_STATE, action) {
     switch (action.type) {
         case SET_LOADING:
             return { ...state, loading: true }
         case UNSET_LOADING:
             return { ...state, loading: false }
         case SET_ERROR:
-            return { ...state, error: true }
+            return { ...state, loading: false, error: true }
         default:
             return {
+                ...state,
                 loading: null,
-                error: null,
             }
     }
 }
