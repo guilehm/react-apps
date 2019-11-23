bae669136ded17f844992906bd8dc3a9e7f99c92
guilherme <guile.hm@hotmail.com>
2019-11-22 22:59:04 -0300

    setup redux thunk

--- games-app/src/index.js
+++ games-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import storage from 'redux-persist/lib/storage'
 import reducer from './reducers'
 import { persistReducer, persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
+import thunk from 'redux-thunk'
 
 
 const persistConfig = {
@@ -15,7 +16,7 @@ const persistConfig = {
 }
 const persistedReducer = persistReducer(persistConfig, reducer)
 
-const store = createStore(persistedReducer)
+const store = applyMiddleware(thunk)(createStore)(persistedReducer)
 const persistor = persistStore(store)
 
 ReactDOM.render(
