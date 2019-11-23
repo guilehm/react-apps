eed893773c428fe67c17deb892ecd5d847239cf1
guilherme <guile.hm@hotmail.com>
2019-11-22 21:39:17 -0300

    refactor game list

--- games-app/src/components/GameList.jsx
+++ games-app/src/components/GameList.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-
 import { VisibleGameItem as Game } from '../containers/GameItem.container'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }))
 
 
 const GameList = ({ games }) => {
+    const classes = useStyles()
+
     return (
-        <ul>
-            {games.map(game =>
-                <li><Game {...game} /></li>
-            )}
-        </ul>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+                {games.map(game =>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Game {...game} />
+                    </Grid>
+                )}
+            </Grid>
+        </div>
     )
 }
 
