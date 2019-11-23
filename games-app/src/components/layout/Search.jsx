a821cc211ea5a87c14b61cb69467264251852480
guilherme <guile.hm@hotmail.com>
2019-11-23 14:06:55 -0300

    remove form to simplify component

--- games-app/src/components/layout/Search.jsx
+++ games-app/src/components/layout/Search.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: 200,
-        },
-    },
-}))
 
 
 const Search = ({ actions, loading, error }) => {
-    const classes = useStyles()
 
     function handleSubmit(e) {
         const name = e.target.value.trim()
@@ -31,18 +20,16 @@ const Search = ({ actions, loading, error }) => {
     }
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
-            <TextField
-                error={error}
-                helperText={
-                    error ? 'Ops... Game not found' : ''
-                }
-                id="outlined-basic"
-                label={label()}
-                onBlur={handleSubmit}
-                autoFocus={true}
-                variant="outlined" />
-        </form>
+        <TextField
+            error={error}
+            helperText={
+                error ? 'Ops... Game not found' : ''
+            }
+            id="outlined-basic"
+            label={label()}
+            onBlur={handleSubmit}
+            autoFocus={true}
+            variant="outlined" />
     )
 }
 
