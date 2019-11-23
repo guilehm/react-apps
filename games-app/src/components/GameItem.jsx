9061bcfb921e46908c9c1280cf16d4391b1862b8
guilherme <guile.hm@hotmail.com>
2019-11-22 23:00:34 -0300

    simplify game item component

--- games-app/src/components/GameItem.jsx
+++ games-app/src/components/GameItem.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
-import Rawg from '../services/api'
-import { useEffect } from 'react'
 
-
-const api = new Rawg()
 
 const Game = props => {
-    useEffect(() => {
-        const fetchGameData = async () => {
-             // o ID abaixo está hardcoded, mas vou buscar das props
-            const result = await api.getGameData(3498)
-            // preciso do "name" que vem nas props para mandar para o action
-            // posteriormente será mudado para o ID
-            props.actions.populateGame(props.name, result.data)
-        }
-        fetchGameData()
-    }, [props.actions, props.name])
-
-
     return (
         <React.Fragment>
             <span>{props.name}</span>
