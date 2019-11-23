dfce0daa4c3b510a38ba24593e799b5a4ee53e89
guilherme <guile.hm@hotmail.com>
2019-11-23 13:29:38 -0300

    create get loading selector

--- games-app/src/selectors/index.js
+++ games-app/src/selectors/index.js
@@ -6,3 +6,9 @@ export const getGameList = createSelector(
     getGames,
     games => games
 )
+
+const getLoading = state => state.layout.loading
+export const getLoadingStatus = createSelector(
+    getLoading,
+    loading => loading
+)
