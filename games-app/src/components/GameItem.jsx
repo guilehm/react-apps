0a91c594f0c7a017f3df002dd7c32fd209de9a89
guilherme <guile.hm@hotmail.com>
2019-11-22 23:33:07 -0300

    improve game item component

--- games-app/src/components/GameItem.jsx
+++ games-app/src/components/GameItem.jsx
@@ -4,9 +4,19 @@ import React from 'react'
 const Game = props => {
     return (
         <React.Fragment>
-            <span>{props.name}</span>
+            <div className="">
+                <h3>{props.name}</h3>
+                <img
+                    src={props.background_image}
+                    alt={props.name}
+                    style={imgStyle}/>
+            </div>
         </React.Fragment>
     )
+}
+
+const imgStyle = {
+    maxWidth: '300px'
 }
 
 export default Game
