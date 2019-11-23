8c74714432f98bed967c66f0c351e04e3e427338
guilherme <guile.hm@hotmail.com>
2019-11-22 22:12:59 -0300

    create case for delete game at reducer

--- games-app/src/reducers/games.js
+++ games-app/src/reducers/games.js
@@ -1,5 +1,6 @@
 import {
     ADD_GAME,
+    DELETE_GAME,
 } from '../constants/ActionTypes'
 
 
@@ -8,6 +9,8 @@ export default function games(state = [], action) {
     switch (action.type) {
         case ADD_GAME:
             return [...state, { ...action.payload }]
+        case DELETE_GAME:
+            return state.filter(game => game.id !== action.id)
         default:
             return state
     }
