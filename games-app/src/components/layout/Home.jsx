34801ac1e718eaac1d7601754cfb14d9cecf034a
guilherme <guile.hm@hotmail.com>
2019-11-23 19:37:59 -0300

    import search list container

--- games-app/src/components/layout/Home.jsx
+++ games-app/src/components/layout/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Search from '../../containers/Search.container'
 import VisibleGameList from '../../containers/GameList.container'
+import SearchList from '../../containers/SearchList.container'
 
 const Home = () => {
     return (
         <React.Fragment>
             <Search />
+            <SearchList />
             <VisibleGameList />
         </React.Fragment>
     )
