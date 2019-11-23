0259fdc0dbe0bda143680ba41887226a4cfc2638
guilherme <guile.hm@hotmail.com>
2019-11-23 10:06:47 -0300

    create set error case in reducer

--- games-app/src/reducers/layout.js
+++ games-app/src/reducers/layout.js
@@ -1,16 +1,22 @@
 import {
     SET_LOADING,
     UNSET_LOADING,
+    SET_ERROR,
 } from '../constants/ActionTypes'
 
 
 export default function layout(state = null, action) {
     switch (action.type) {
         case SET_LOADING:
-            return { loading: true }
+            return { ...state, loading: true }
         case UNSET_LOADING:
-            return { loading: false }
+            return { ...state, loading: false }
+        case SET_ERROR:
+            return { ...state, error: true }
         default:
-            return { loading: null }
+            return {
+                loading: null,
+                error: null,
+            }
     }
 }
