1be1db4cef1d923649ae6a1c899a2465ae8f3aef
guilherme <guile.hm@hotmail.com>
2019-11-23 19:38:58 -0300

    import search items to reducer

--- games-app/src/reducers/index.js
+++ games-app/src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import games from './games'
 import layout from './layout'
+import searchItems from './search-items'
 
 
 const rootReducer = combineReducers({
     games,
     layout,
+    searchItems,
 })
 
 export default rootReducer
