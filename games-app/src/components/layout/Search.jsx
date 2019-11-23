26667bc8d0eb2eaa714186e1a11b420f37b81196
guilherme <guile.hm@hotmail.com>
2019-11-23 10:19:04 -0300

    fix identation

--- games-app/src/components/layout/Search.jsx
+++ games-app/src/components/layout/Search.jsx
@@ -3,12 +3,12 @@ import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: 200,
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: 200,
+        },
     },
-  },
 }))
 
 
