066ccd37c7af502b594eefd5a781137f8f375a19
guilherme <guile.hm@hotmail.com>
2019-11-22 21:39:26 -0300

    change layout for search field

--- games-app/src/components/layout/Search.jsx
+++ games-app/src/components/layout/Search.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}))
 
 
 const Search = ({ actions }) => {
+    const classes = useStyles()
+
     function handleSubmit(e) {
         const name = e.target.value.trim()
         if (name) actions.addGame({ name })
@@ -9,12 +22,13 @@ const Search = ({ actions }) => {
     }
 
     return (
-        <input
-            type="text"
-            placeholder="Enter game name..."
-            autoFocus={true}
-            onBlur={handleSubmit}
-        />
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField
+                id="outlined-basic"
+                label="Game name"
+                onBlur={handleSubmit}
+                variant="outlined" />
+        </form>
     )
 }
 
