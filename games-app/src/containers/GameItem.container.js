481f8b8deb1f3836f67dc14801adac529fc03690
guilherme <guile.hm@hotmail.com>
2019-11-23 23:12:25 -0300

    remove extra line

--- games-app/src/containers/GameItem.container.js
+++ games-app/src/containers/GameItem.container.js
@@ -8,7 +8,6 @@ const mapStateToProps = (state, ownProps) => {
     return {...ownProps}
   }
 
-
 const mapDispatchToProps = dispatch => ({
     actions: bindActionCreators(GameActions, dispatch)
 })
