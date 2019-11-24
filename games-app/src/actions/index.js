cf092635f8e40e6d74663e732f1c059899b4957e
guilherme <guile.hm@hotmail.com>
2019-11-24 00:07:06 -0300

    fetch game detail instead of results

--- games-app/src/actions/index.js
+++ games-app/src/actions/index.js
@@ -5,11 +5,16 @@ import Rawg from '../services/api'
 const API = new Rawg()
 
 export const addGame = game => {
+    let handleSuccess = res => {
+        let id = res.data.results[0].id
+        return API.getGameData(id)
+    }
     return dispatch => {
         API.searchByName(game.name)
+            .then(handleSuccess)
             .then(res => dispatch({
                 type: types.ADD_GAME,
-                payload: res.data.results[0]
+                payload: res.data
             }))
     }
 }
