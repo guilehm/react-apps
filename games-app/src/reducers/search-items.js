4082f2cbdaa31b505d60e3ae83f68278259dedad
guilherme <guile.hm@hotmail.com>
2019-11-24 13:30:02 -0300

    add case for clear search items at reducers

--- games-app/src/reducers/search-items.js
+++ games-app/src/reducers/search-items.js
@@ -1,5 +1,6 @@
 import {
     ADD_SEARCH_ITEMS,
+    CLEAR_SEARCH_ITEMS,
 } from '../constants/ActionTypes'
 
 
@@ -7,6 +8,8 @@ export default function searchItems(state = [], action) {
     switch (action.type) {
         case ADD_SEARCH_ITEMS:
             return [...action.payload]
+        case CLEAR_SEARCH_ITEMS:
+            return []
         default:
             return state
     }
