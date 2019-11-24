d102ce5bf1f4f2d606cbf846e43a498770bc7436
guilherme <guile.hm@hotmail.com>
2019-11-24 00:07:17 -0300

    improve game item component

--- games-app/src/components/GameItem.jsx
+++ games-app/src/components/GameItem.jsx
@@ -1,22 +1,109 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+const useStyles = makeStyles(theme => ({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
 
 const Game = props => {
-    return (
-        <React.Fragment>
-            <div className="">
-                <h3>{props.name}</h3>
-                <img
-                    src={props.background_image}
-                    alt={props.name}
-                    style={imgStyle}/>
-            </div>
-        </React.Fragment>
-    )
-}
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
 
-const imgStyle = {
-    maxWidth: '300px'
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card className={classes.card}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={props.name}
+            // subheader="September 14, 2016"
+            />
+            <CardMedia
+                className={classes.media}
+                image={props.background_image}
+                title={props.name}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {
+                        props.description_raw.length <= 180 ?
+                            props.description_raw :
+                            props.description_raw.slice(0, 180).trim() + '...'
+                    }
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography paragraph>{props.description_raw}</Typography>
+                </CardContent>
+            </Collapse>
+        </Card>
+    )
 }
 
 export default Game
