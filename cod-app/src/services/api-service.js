32bccd233b356e20613c3e49e96c8f581903f872
guilherme <guile.hm@hotmail.com>
2020-05-18 22:33:46 -0300

    improve app, component and service

--- cod-app/src/services/api-service.js
+++ cod-app/src/services/api-service.js
@@ -1,16 +1,18 @@
 import axios from 'axios'
 
 
-class Cod {
+class CodApi {
     constructor() {
-        this.baseUrl = 'https://api.tracker.gg/api/v2/warzone/standard/profile'
+        // this.baseUrl = 'https://api.tracker.gg/api/v2/warzone/standard/profile'
+        this.baseUrl = 'http://localhost:9999'
     }
 
     getProfileData(platform, username) {
         const url = `${this.baseUrl}/${platform}/${username}`
+        console.log(`'fetching' ${url}`)
         return axios.get(url)
     }
 }
 
 
-export default Cod
+export default CodApi
