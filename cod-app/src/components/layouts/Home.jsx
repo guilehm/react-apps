6036f7c58880fcb3de5eb630f568713ca3995a93
guilherme <guile.hm@hotmail.com>
2020-05-20 10:07:55 -0300

    refactor home component

--- cod-app/src/components/layouts/Home.jsx
+++ cod-app/src/components/layouts/Home.jsx
@@ -1,56 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import PlayerDetail from '../../components/Player'
 import Header from './Header'
-
+import ChartGroup from '../ChartGroup'
 
 const Home = () => {
-
-    const [playerList, setPlayerList] = useState([])
-    const [username, setUsername] = useState('')
-    const [platform, setPlatform] = useState('psn')
-
-    const handleSubmit = () => {
-        if (!username) return
-        setPlayerList([...playerList, {username, platform}])
-        setUsername('')
-    }
 
     return (
         <div>
             <Header />
-            <input
-                value={username}
-                name='username'
-                type='text'
-                placeholder='username...'
-                autoFocus
-                onChange={e => setUsername(e.target.value)}
-            />
-            <select
-                name='platform'
-                value={platform}
-                onChange={e => setPlatform(e.target.value)}>
-                <option value='psn'>psn</option>
-                <option value='pc'>pc</option>
-                <option value='battlenet'>battlenet</option>
-                <option value='xbl'>xbl</option>
-                <option value='atvi'>activision</option>
-            </select>
-            <button
-                name='save'
-                onClick={() => handleSubmit()}>
-                    save
-            </button>
-
-            <ul>
-                {playerList.map(player => (
-                    <PlayerDetail
-                        key={player.username}
-                        name={player.username}
-                        platform={player.platform} />
-                ))}
-            </ul>
+            <ChartGroup />
         </div>
     )
 }
