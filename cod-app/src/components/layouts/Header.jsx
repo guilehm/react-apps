245c9f9fcf34e03f6333f2265ad0cdbc0ceb06e8
guilherme <guile.hm@hotmail.com>
2020-05-20 00:53:26 -0300

    create header component

---
+++ cod-app/src/components/layouts/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => (
+    <React.Fragment>
+        <h1>Hello!</h1>
+        <p>thanks for using our app</p>
+    </React.Fragment>
+)
+
+
+export default Header
