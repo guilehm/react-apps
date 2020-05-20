90841304be00acbfb7098de3424d8bc1ca800ae0
guilherme <guile.hm@hotmail.com>
2020-05-20 10:20:14 -0300

    setup redux

--- cod-app/src/index.js
+++ cod-app/src/index.js
@@ -3,9 +3,29 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import reducer from './reducers'
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducer = persistReducer(persistConfig, reducer)
+const store = applyMiddleware(thunk)(createStore)(persistedReducer)
+const persistor = persistStore(store)
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
     document.getElementById('root')
 )
