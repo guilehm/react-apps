281e296a7e0719337a0a6568194463887fc4746a
guilherme <guile.hm@hotmail.com>
2020-05-20 10:20:22 -0300

    create base reducers

---
+++ cod-app/src/reducers/index.js
@@ -0,0 +1,10 @@
+import players from './players'
+import { combineReducers } from 'redux'
+
+
+const rootReducer = combineReducers({
+    players,
+})
+
+
+export default rootReducer
---
+++ cod-app/src/reducers/players.js
@@ -0,0 +1,9 @@
+const players = (state = [], action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+
+export default players
