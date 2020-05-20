e4deff2f368c040b9e13f7ec5badd9e43d5a2945
guilherme <guile.hm@hotmail.com>
2020-05-19 22:09:02 -0300

    improve home layout

--- cod-app/src/components/layouts/Home.jsx
+++ cod-app/src/components/layouts/Home.jsx
@@ -6,6 +6,14 @@ import PlayerDetail from '../../components/Player'
 const Home = () => {
 
     const [playerList, setPlayerList] = useState([])
+    const [username, setUsername] = useState('')
+    const [platform, setPlatform] = useState('psn')
+
+    const handleSubmit = () => {
+        if (!username) return
+        setPlayerList([...playerList, {username, platform}])
+        setUsername('')
+    }
 
     return (
         <div>
@@ -13,11 +21,37 @@ const Home = () => {
             <p>thanks for using our app</p>
 
             <input
-                type="text"
-                placeholder="Enter username..."
+                value={username}
+                name='username'
+                type='text'
+                placeholder='username...'
                 autoFocus
+                onChange={e => setUsername(e.target.value)}
             />
-            {/* <PlayerDetail name='Guilehm91' platform='psn' /> */}
+            <select
+                name='platform'
+                value={platform}
+                onChange={e => setPlatform(e.target.value)}>
+                <option value='psn'>psn</option>
+                <option value='pc'>pc</option>
+                <option value='battlenet'>battlenet</option>
+                <option value='xbl'>xbl</option>
+                <option value='atvi'>activision</option>
+            </select>
+            <button
+                name='save'
+                onClick={() => handleSubmit()}>
+                    save
+            </button>
+
+            <ul>
+                {playerList.map(player => (
+                    <PlayerDetail
+                        key={player.username}
+                        name={player.username}
+                        platform={player.platform} />
+                ))}
+            </ul>
         </div>
     )
 }
