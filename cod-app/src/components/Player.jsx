56e623ea99d2735766305dd127a0a0ee7ba3ba2d
guilherme <guile.hm@hotmail.com>
2020-05-19 21:45:41 -0300

    improve player detail component

--- cod-app/src/components/Player.jsx
+++ cod-app/src/components/Player.jsx
@@ -1,25 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CodApi from '../services/api-service'
 
 const PlayerDetail = props => {
 
     const [data, setData] = useState()
+    useEffect(() => {
+        (async () => {
+            const api = new CodApi()
+            const response = await api.getProfileData(props.platform, props.name)
+            setData(response.data)
+        })();
+      }, [props.name, props.platform]);
 
-    const fetchData = async () => {
-        const api = new CodApi()
-        const response = await api.getProfileData(props.platform, props.name)
-        setData(response.data)
-    }
+    const battleRoyale = data ? data.data.segments
+        .find(s => s.metadata.name === 'Battle Royale') : null
 
     return (
         <div>
-            <h2>Hey! {props.name}</h2>
-            <p>{data ? JSON.stringify(data.data.platformInfo) : null}</p>
-            <button
-                name='search'
-                onClick={fetchData}>
-                    buscar
-            </button>
+            <h2>Player: {props.name}</h2>
+            {
+                battleRoyale ?
+                <ul>
+                    <li>kills: {battleRoyale.stats.kills.value}</li>
+                    <li>deaths: {battleRoyale.stats.deaths.value}</li>
+                    <li>wins: {battleRoyale.stats.wins.value}</li>
+                    <li>kdRatio: {
+                        (battleRoyale.stats.kills.value / battleRoyale.stats.deaths.value).toFixed(2)
+                    }</li>
+                </ul>
+                : null
+            }
         </div>
     )
 
